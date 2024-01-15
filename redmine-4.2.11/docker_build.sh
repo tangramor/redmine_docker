7f3a7b8a4ecb5b851b1888db85b76dcdeab07e3e
wangjunhua <wangjunhua@longtugame.com>
2024-01-15 10:52:44 +0800

    add 4.2.11

---
+++ redmine-4.2.11/docker_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build --build-arg RUBY_MIRROR="https://cache.ruby-china.com/pub/ruby" \
+    --build-arg GEM_MIRROR="https://gems.ruby-china.com" \
+    -t tangramor/redmine:4.2.11 .
