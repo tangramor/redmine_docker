3ff0675d33bf1f52c587b2e1128f2b04f1593051
tangramor <tangramor@gmail.com>
2024-11-04 17:58:16 +0800

    update 5.1.x to 5.1.4

--- redmine-5.1.x/docker_build.sh
+++ redmine-5.1.x/docker_build.sh
@@ -2,6 +2,6 @@
 
 docker build --build-arg RUBY_MIRROR="https://cache.ruby-china.com/pub/ruby" \
     --build-arg GEM_MIRROR="https://gems.ruby-china.com" \
-    -t tangramor/redmine:5.1.2 .
+    -t tangramor/redmine:5.1.4 .
 
-docker tag tangramor/redmine:5.1.2 tangramor/redmine
+docker tag tangramor/redmine:5.1.4 tangramor/redmine
